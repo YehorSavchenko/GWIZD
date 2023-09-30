9caf77f047a241a4040f060d744d658fd79b969e
sasha <alexa.ishchuk@gmail.com>
2023-09-30 18:26:36 +0200

    Added user camera permission to AndroidManifest.xml

--- app/src/main/java/com/centarius/gwizd/activity/MainActivity.java
+++ app/src/main/java/com/centarius/gwizd/activity/MainActivity.java
@@ -1,6 +1,8 @@
 package com.centarius.gwizd.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,9 +10,18 @@ import com.centarius.gwizd.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button openCameraButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        openCameraButton = findViewById(R.id.openCameraBtn);
+
+        openCameraButton.setOnClickListener(view -> {
+            Intent intent = new Intent(this, CameraActivity.class);
+            startActivity(intent);
+        });
     }
 }
