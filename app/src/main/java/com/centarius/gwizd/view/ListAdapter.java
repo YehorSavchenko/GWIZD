cd4aaaea8c4343d42df94146fd364be9cb2ccfc2
Yehor <egor07030303@gmail.com>
2023-09-30 22:36:59 +0200

    Change AnimalSpotted and location Add recycle view and mock list of items

--- app/src/main/java/com/centarius/gwizd/view/ListAdapter.java
+++ app/src/main/java/com/centarius/gwizd/view/ListAdapter.java
@@ -4,21 +4,23 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.centarius.gwizd.R;
+import com.centarius.gwizd.model.AnimalSpotted;
 
 import java.util.List;
 
 public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ListViewHolder> {
 
-    private final List<String> dataList;
+    private final List<AnimalSpotted> dataList;
     private final LayoutInflater layoutInflater;
 
-    public ListAdapter(Context context, List<String> dataList) {
+    public ListAdapter(Context context, List<AnimalSpotted> dataList) {
         this.dataList = dataList;
         this.layoutInflater = LayoutInflater.from(context);
     }
@@ -26,14 +28,20 @@ public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ListViewHolder
     @NonNull
     @Override
     public ListViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View itemView = layoutInflater.inflate(R.layout.list_item, parent, false);
+        View itemView = layoutInflater.inflate(R.layout.list_animal_item, parent, false);
         return new ListViewHolder(itemView);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ListViewHolder holder, int position) {
-        String data = dataList.get(position);
-        holder.textView.setText(data);
+        AnimalSpotted animalSpotted = dataList.get(position);
+
+        holder.tvAnimalTypeAndStatus.setText(animalSpotted.getAnimalType() + " (" + animalSpotted.getAnimalStatus().toString() + ")");
+        holder.tvLocation.setText("Location: " + animalSpotted.getLocation());  // Assuming Location has a suitable toString method
+        holder.tvTimestamp.setText("Timestamp: " + animalSpotted.getTimestamp());
+
+        // Assuming you will set up image loading for the ImageView
+        // For example, if using Glide: Glide.with(holder.ivArrow.getContext()).load(animalSpotted.getImageUrl()).into(holder.ivArrow);
     }
 
     @Override
@@ -42,11 +50,17 @@ public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ListViewHolder
     }
 
     public static class ListViewHolder extends RecyclerView.ViewHolder {
-        final TextView textView;
+        final TextView tvAnimalTypeAndStatus;
+        final TextView tvLocation;
+        final TextView tvTimestamp;
+        final ImageView ivArrow; // For the clickable arrow
 
         public ListViewHolder(@NonNull View itemView) {
             super(itemView);
-            textView = itemView.findViewById(R.id.text_view);  // Assuming that you have a TextView with id 'textView' in your list_item layout
+            tvAnimalTypeAndStatus = itemView.findViewById(R.id.tvAnimalTypeAndStatus);
+            tvLocation = itemView.findViewById(R.id.tvLocation);
+            tvTimestamp = itemView.findViewById(R.id.tvTimestamp);
+            ivArrow = itemView.findViewById(R.id.ivArrow);
         }
     }
 }
