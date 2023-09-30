c94190ddd2b0a5ca9396d6d6f5fea222cf561f71
Yehor <egor07030303@gmail.com>
2023-09-30 18:52:28 +0200

    change startActivityForResult to mGetContent.launch(photoIntent) which is not deprecated

---
+++ app/src/main/java/com/centarius/gwizd/activity/CameraActivity.java
@@ -0,0 +1,109 @@
+package com.centarius.gwizd.activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.icu.text.SimpleDateFormat;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
+
+import com.centarius.gwizd.R;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Date;
+import java.util.Locale;
+
+public class CameraActivity extends AppCompatActivity {
+
+    private static final int CAMERA_PERMISSION_REQUEST_CODE = 777;
+    static final int REQUEST_IMAGE_CAPTURE_CODE = 1;
+
+    Uri imageUri;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.camera_activity);
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.CAMERA}, CAMERA_PERMISSION_REQUEST_CODE);
+        } else {
+            getPicture();
+        }
+    }
+
+    private void getPicture() {
+        Intent photoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (photoIntent.resolveActivity(getPackageManager()) != null) {
+            File photoFile = null;
+            try {
+                SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd_HHmmss", new Locale("pl", "PL"));
+                String timeStamp = sdf.format(new Date());
+                String imageFileName = "JPEG_" + timeStamp + "_";
+                File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+                photoFile = File.createTempFile(
+                        imageFileName,  /* prefix */
+                        ".jpg",         /* suffix */
+                        storageDir      /* directory */
+                );
+                // Save a file: path for use with ACTION_VIEW intents
+                imageUri = FileProvider.getUriForFile(this, "com.centarius.gwizd.fileprovider", photoFile);
+            } catch (IOException ex) {
+                Log.e("CameraError", "Error occurred while creating the File", ex);
+            }
+
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                photoIntent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+                mGetContent.launch(photoIntent);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        // If request is cancelled, the result arrays are empty.
+        if (requestCode == CAMERA_PERMISSION_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                getPicture();
+            } else {
+                Toast.makeText(this, "Camera permission is necessary to take a photo.",
+                        Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE_CODE && resultCode == RESULT_OK) {
+            // The image is saved in the imageUri, do something with it, for example:
+            ImageView photoView = findViewById(R.id.photoView);
+            photoView.setImageURI(imageUri);
+        }
+    }
+
+    private final ActivityResultLauncher<Intent> mGetContent = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                if (result.getResultCode() == Activity.RESULT_OK) {
+                    // The image is saved in the imageUri, do something with it, for example:
+                    ImageView photoView = findViewById(R.id.photoView);
+                    photoView.setImageURI(imageUri);
+                }
+            });
+}
