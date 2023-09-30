e23871a0cd96fdf7f2176ca92abc43805dfd7b48
Yehor <egor07030303@gmail.com>
2023-09-30 20:48:23 +0200

    Add BottomNavigationView

--- app/src/main/java/com/centarius/gwizd/activity/MainActivity.java
+++ app/src/main/java/com/centarius/gwizd/activity/MainActivity.java
@@ -2,18 +2,15 @@ package com.centarius.gwizd.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.MenuItem;
 import android.widget.Button;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.centarius.gwizd.R;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.navigation.NavigationBarView;
 import com.centarius.gwizd.database.AnimalDao;
 import com.centarius.gwizd.model.AnimalSpotted;
 import com.centarius.gwizd.model.Location;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.FirebaseApp;
 
 public class MainActivity extends AppCompatActivity {
@@ -45,19 +42,16 @@ public class MainActivity extends AppCompatActivity {
     protected void setBottomNavigationView(){
         // Setting action_camera as the default selected item
         bottomNavigationView.setSelectedItemId(R.id.action_camera);
-        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                int id = item.getItemId();
-                if (id == R.id.action_list) {
-                    // Handle Home click
-                } else if (id == R.id.action_camera) {
-                    // Handle Search click
-                } else if (id == R.id.action_profile) {
-                    // Handle Profile click
-                }
-                return true;
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            int id = item.getItemId();
+            if (id == R.id.action_list) {
+                // Handle Home click
+            } else if (id == R.id.action_camera) {
+                // Handle Search click
+            } else if (id == R.id.action_profile) {
+                // Handle Profile click
             }
+            return true;
         });
     }
 }
